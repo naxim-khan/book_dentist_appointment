ee5ff00a51ff4b441a474db0f6f60049256add2d
Nazeem khan <120269101+naxim-khan@users.noreply.github.com>
2024-06-25 02:28:41 +0500

    Update settings.py

--- fyp_web_app/settings.py
+++ fyp_web_app/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'conner.cole911@gmail.com'
-EMAIL_HOST_PASSWORD = 'qtirxxpapyiedtyo'
+EMAIL_HOST_USER = 'yourGmail'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True  
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
